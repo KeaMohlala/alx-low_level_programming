5de76f4c3e00063af6de2190fbb37ef33275be41
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-05 12:47:15 -0700

    add description and remove white spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(void)
-{
+{/**
+   This is a descrption for the main function
+   */
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0);
+
+	return (0)
 }
