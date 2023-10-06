1abbac10e01f7427660f16804325f4dba826ba49
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-06 13:57:28 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+
+	}
+
+	putchar('\n');
+	return (0);
+}
