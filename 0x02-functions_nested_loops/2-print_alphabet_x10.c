0fe993141fd1eaae4ab3cba19d040b36f530d434
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-10 10:09:49 -0700

    task 2 print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char c, ch;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar ('\n');
+	}
+}
