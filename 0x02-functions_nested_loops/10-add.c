e34f5770cd275c3ca81ddf493c5ab7018152ebfb
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-11 05:06:00 -0700

    task 10 add two intergers and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * Return: results of the addition
+ * @x: first interger input
+ * @y: second interger input
+ */
+
+int add(int x, int y)
+
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
