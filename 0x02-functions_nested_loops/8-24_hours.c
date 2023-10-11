a7ae2a946d8fa79a5de278fec49539626417c636
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-11 11:48:21 -0700

    task 8 print every minute in 24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,23 @@
-#include "main"
+#include "main.h"
+
 /**
- *
+ *jack_bauer - list all min for 24 hours
+ */
+
+void jack_bauer(void)
+{
+	int hh, mm;
+
+	for (hh = 0; hh <= 23; hh++)
+	{
+		for (mm = 0; mm <= 59; mm++)
+		{
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(58);
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
