89e914eefbd2bf4a521927c58d15c03731ac6257
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-12 05:13:39 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,13 +13,13 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (b > a && b > c)
 {
 largest = b;
+}
+else if (c > a && c > b)
+{
+largest = c;
 }
 else
 {
