fada14435cd08ea33f993949fb2098378fdb6272
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-13 06:52:17 -0700

    task 9 FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	char c[5] = "Fizz";
+	char b[5] = "Buzz";
+	char a[9] = "FizzBuzz";
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("%s ", a);
+		else if (n % 5 == 0)
+			printf("%s ", b);
+		else if (n % 3 == 0)
+			printf("%s ", c);
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
+}
