a688dad58d7836c0640dc1795374dd85b1b79ad7
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-13 11:21:08 -0700

    edge cases test 2 added more statements to print fizz and buzz; changed spacing to before and not after the word/number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,14 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
-			printf("%s ", a);
-		else if (n % 5 == 0)
-			printf("%s ", b);
-		else if (n % 3 == 0)
-			printf("%s ", c);
-		else if (n != 0)
-			printf("%d ", n);
+			printf(" %s", a);
+		else if ((n % 5 == 0) && (n % 3 != 0))
+			printf(" %s", b);
+		else if ((n % 3 == 0) && (n % 5 != 0))
+			printf(" %s", c);
+		else if (n == 1)
+			printf("%d", n);											  else
+			printf(" %d", n);
 	}
 	printf("\n");
 	return (0);
