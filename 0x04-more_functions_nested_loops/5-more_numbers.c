4892d4627426cd2a2df6e958f73060e9714ed3a0
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-13 05:07:52 -0700

    Revert " seperate if statements for the single and double digits"
    This reverts commit 14de37b1d723a86732de056e1bdee90b284f4e0b.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,10 +15,8 @@ void more_numbers(void)
 			if (b >= 10)
 			{
 			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
 			}
-			else if (b < 10)
-			_putchar(b + '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
