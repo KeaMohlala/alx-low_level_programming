4afb9ffaf5b63d6c1941106adbf050a0d3c25df4
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-13 04:14:37 -0700

    task 6 move while loop inside else statement at end

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,17 @@
 
 void print_line(int n)
 {
-	while (n--)
+	if (n <= 0)
 	{
-		if (n <= 0)
-			_putchar('\n');
-		else
-			_putchar('_');
-		       /* _putchar('\n');*/
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		while (n--)
+		{
+		_putchar('_');
+		}
+		_putchar('\n');
+	}
+	/*_putchar('\n');*/
 }
