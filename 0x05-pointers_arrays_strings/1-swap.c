c900a8e2567fddfba2731737f7b409002a3d04a0
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-16 14:06:01 -0700

    task 1 swap value of intergers using pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of intergers using pointers
+ * @a: pointer to change value of a to 42
+ * @b: pointer to change value of b to 98
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
