0178a6e9375d5e83e11b067b1deeb14529be4def
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-16 14:58:21 -0700

    task 1 correct code to swap any two intergers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int - swap values of intergers using pointers
+ *swap_int -wap values of intergers using pointers
  * @a: pointer to change value of a to 42
  * @b: pointer to change value of b to 98
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
