d08c9f94864fb4f0a539a313c3f88bf29ac6bc2f
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-16 13:51:53 -0700

    task 0 set interger value to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set interger to 98
+ * @n:pointer of the interger to set to 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define main_h
 
 int _putchar(char);
+void reset_to_98(int *n);
 
 #endif
 
