72dc271bd5b299593c333dd8db35b315c0844d53
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-17 16:46:50 -0700

    for array with index

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,19 +9,15 @@
 
 void print_array(int *a, int n)
 {
-	if (n > 0)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *(a++));
-		n--;
-	}
-	if (n <= 0)
-	{
-		printf(",");
-		return;
-	}
-	while (n--)
-	{
-		printf(", %d", *(a++));
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
