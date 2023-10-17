f0ff9dcb95a71b8f0c76e1d42caf9da8e3261fcd
KeaMohlala <keamogetsemhll@gmail.com>
2023-10-17 15:28:22 -0700

    task 8 print numeber of elemnts of an integer arry

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array- print n elements of an array of integers
+ * @a: pointer to the array
+ * @n:number of elements of the array to be printed
+ */
+
+void print_array(int *a, int n)
+{
+	if (n > 0)
+	{
+		printf("%d", *(a++));
+		n--;
+	}
+	while (n--)
+	{
+		printf(", %d", *(a++));
+	}
+	printf("\n");
+}
