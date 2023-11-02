a9a893c8582d0fa5cfbca3247b97b5e4a742d97b
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-02 12:53:19 -0700

    added own strlen function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+/**
+* _strlen - function to determine length of a string
+* Return: length of the string
+* @str: pointer to the first character of the string
+*/
+
+int _strlen(char *str)
+{
+int length = 0;
+
+while (*str != '\0')
+{
+length++;
+str++;
+}
+return (length);
+}
 
 /**
 * string_nconcat - function that concetenates two strings
@@ -15,21 +34,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i = 0;
 unsigned int j = 0;
 char *ptr;
-unsigned int len_s1 = strlen(s1);
-/*unsigned int len_s2 = strlen(s2)*/
+unsigned int len_s1 = _strlen(s1);
+unsigned int len_s2 = _strlen(s2);
 
-if (s1 == 0)
+if (s1 == NULL)
 s1 = "";
-if (s2 == 0)
+if (s2 == NULL)
 s2 = "";
 
- /*if (n >= len_s2)*/
- /*ptr = malloc(len_s1 + len_s2 + 1)*/
- /*else*/
-
+if (n >= len_s2)
+ptr = malloc(len_s1 + len_s2 + 1);
+else
 ptr = malloc(len_s1 + n + 1);
-if (ptr == 0)
-return (0);
+if (ptr == NULL)
+return (NULL);
 
 
 while (s1[i])
