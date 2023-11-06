05e5824c92c177b9180febcbd7ae3f00fa2dc07a
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-06 13:44:42 -0800

    task 5 free allocated memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - function to free dynamically allocated memory
+ * @d: pointer to allocated memory
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->owner);
+}
