d077fecc0c4d5ba62066ab506d28e4676979d82c
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-08 15:09:52 -0800

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+* main -point of entry
+* @argc: number of arguments on the command line
+* @argv: pointer to array of pointers with address values of the arguments
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+int calc;
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[3]);
+int (*operator)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+operator = get_op_func(argv[2]);
+
+if (operator == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+calc = operator(num1, num2);
+
+printf("%d\n", calc);
+
+return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - adds two integers
+* @a: first number
+* @b: second number
+* Return: sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - subtracts two integers
+* @a: first number
+* @b: second number
+* Return: Difference between a and b
+*/
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - multiplies two integers
+* @a: first number
+* @b: second number
+* Return: Product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - divides two integers
+* @a: first number
+* @b: second number
+* Return: result of the division of a and b
+*/
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod - remainder after division of two integers
+* @a: first number
+* @b: second number
+* Return: remainder of the division of a and b
+*/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
