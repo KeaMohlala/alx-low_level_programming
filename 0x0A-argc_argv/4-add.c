22eaefa9b868243afa30cd2f48aa701599d97aed
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-10 13:42:18 -0800

    check each char of the argument

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,41 +4,41 @@
 #include "main.h"
 
 /**
- * check_digit - checks if character is digit, if not loop stops
- * and next t argument is evaluated
- * @n: array to be searched
- * @count: number of arguments
- * Return: sum of digits or 0 if there is a non-digit
- */
+* check_digit - checks if character is digit, if not loop stops
+* and next t argument is evaluated
+* @n: array to be searched
+* @count: number of arguments
+* Return: sum of digits or 0 if there is a non-digit
+*/
 
 int check_digit(int count, char *n[])
 {
-unsigned int i;
+unsigned int i, j;
 unsigned int add = 0;
 
 for (i = 1; i < (unsigned int) count; i++)
 {
-if (isdigit(*n[i]) != 0)
+for (j = 0; n[i][j] != '\0'; j++)
 {
-add += atoi(n[i]);
-/*printf("%d\n", add);*/
-}
-else
+if (isdigit(n[i][j]) == 0)
 {
 printf("Error\n");
 return (1);
 }
 }
+add += atoi(n[i]);
+}
 printf("%d\n", add);
 return (0);
 }
 
+
 /**
- * main -point of entry
- * @argc: number of arguments on the command line
- * @argv: pointer to array of pointers with address values of the arguments
- * Return: 0
- */
+* main -point of entry
+* @argc: number of arguments on the command line
+* @argv: pointer to array of pointers with address values of the arguments
+* Return: 0
+*/
 
 int main(int argc, char *argv[])
 {
