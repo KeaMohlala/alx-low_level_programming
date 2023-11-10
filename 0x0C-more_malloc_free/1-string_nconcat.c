aa0576a382871d36f94a531f29a4e722446843ec
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-10 11:48:55 -0800

    assign strlen to len after the if statements

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,13 +16,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i = 0;
 unsigned int j = 0;
 char *ptr;
-unsigned int len_s1 = strlen(s1);
-unsigned int len_s2 = strlen(s2);
+unsigned int len_s1, len_s2;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
+
+len_s1 = strlen(s1);
+len_s2 = strlen(s2);
 
 if (n >= len_s2)
 ptr = malloc((len_s1 + len_s2 + 1) * sizeof(char));
