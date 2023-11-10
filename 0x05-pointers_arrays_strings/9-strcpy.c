79d6403d214cfab95e1354bbd87e4d8af7b472b9
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-10 14:16:24 -0800

    print large strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * _strcpy - copies string from source to buffer desination
- * @dest: destination pointer
- * @src: source pointer
- * Return: pointer to the destination
- */
+* _strcpy - copies string from source to buffer desination
+* @dest: destination pointer
+* @src: source pointer
+* Return: pointer to the destination
+*/
 
 char *_strcpy(char *dest, char *src)
 {
-	int length = 0;
-	char *temp;
+char *temp;
 
-	while (src[length] != '\0')
-	{
-		length++;
-		if (length > 98)
-			return (src);
-	}
-
-	temp = dest;
-	while ((*dest++ = *src++) != '\0')
-		;
-
-	return (temp);
+temp = dest;
+while ((*dest++ = *src++) != '\0')
+{
+return (temp);
+}
+return (temp);
 }
