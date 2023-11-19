eac22af1890968ff208c0315b5feebcef7a23f7e
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-19 11:35:48 -0800

    task 0 fix if statement for null str

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,24 +10,19 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-list_t *i = NULL;
 
 while (h != NULL)
 {
-if (h -> str == NULL)
+if (h->str == NULL)
 {
-i = h;
-i -> str = "(nil)";
-i -> len = 0;
-printf("[%d] %s\n", i -> len, i -> str);
+printf("[0] (nil)\n");
 }
 else
 {
-printf("[%d] %s\n", h -> len, h -> str);
+printf("[%d] %s\n", h->len, h->str);
 }
 count++;
-h = h -> next;
-
+h = h->next;
 }
 return (count);
 }
