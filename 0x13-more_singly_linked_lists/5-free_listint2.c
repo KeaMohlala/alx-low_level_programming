e6aab819795db8930b0568434fe0972dde56a9b1
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-20 08:14:04 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_listint2 - frees a linked list and sets head pointer to NULL
+* @head: pointer to head pointer (pointer to first node)
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *ptr;
+listint_t *next;
+
+ptr = *head;
+
+while (ptr != NULL)
+{
+next = ptr->next;
+free(ptr);
+ptr = next;
+}
+*head = NULL;
+}
