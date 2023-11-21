15a37a02d684a7d9c9b254372bbcf3ffc28af099
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-21 12:16:12 -0800

    task 5, use two pointers, free head and pointer to head before while loop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,17 +9,21 @@
 
 void free_listint2(listint_t **head)
 {
+listint_t *ptr;
 listint_t *next;
 
-if (head == NULL)
+if (*head == NULL)
 return;
 
-
-while (*head != NULL)
-{
-next = (*head)->next;
-free(*head);
-*head = next;
-}
+ptr = *head;
+*head = NULL;
 head = NULL;
+
+
+while (ptr != NULL)
+{
+next = ptr->next;
+free(ptr);
+ptr = next;
+}
 }
