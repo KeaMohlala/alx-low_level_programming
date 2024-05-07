5aedf121faad9394870559f9901bd199e301f8a4
KeaMohlala <keamogetsemhll@gmail.com>
2024-05-07 09:58:02 -0700

    changing printing statement

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,23 +3,21 @@
 /**
 * print_array - prints an array of integers
 * @array: array to print
-* @size: size of the array
+* @low: first half of the array
+* @high: second half of the array
 * Return: void
 */
 
-void print_array(int *array, size_t size)
+void print_array(int *array, size_t low, size_t high)
 {
 size_t i;
 
 printf("Searching in array: ");
-for (i = 0; i < size; i++)
+for (i = low; i < high; i++)
 {
-if (i < size - 1)
 printf("%d, ", array[i]);
-else
-printf("%d", array[i]);
 }
-printf("\n");
+printf("%d\n", array[i]);
 }
 
 /**
@@ -32,44 +30,24 @@ printf("\n");
 
 int binary_search(int *array, size_t size, int value)
 {
-size_t i, mid;
+size_t mid;
 size_t low = 0, high = size - 1;
 
 if (array == NULL)
 return (-1);
 
-print_array(array, size);
-while (low < high)
+while (low <= high)
 {
-mid = low + (high - low) / 2;
+print_array(array, low, high);
+
+mid = (high + low) / 2;
+
 if (array[mid] == value)
 return (mid);
-else if (array[mid] < value)
-{
-low = mid + 1;
-printf("Searching in array: ");
-for (i = low; i <= high; i++)
-{
-if (i < high)
-printf("%d, ", array[i]);
-else
-printf("%d", array[i]);
-}
-printf("\n");
-}
-else
-{
+else if (array[mid] > value)
 high = mid - 1;
-printf("Searching in array: ");
-for (i = 0; i <= high; i++)
-{
-if (i < high)
-printf("%d, ", array[i]);
 else
-printf("%d", array[i]);
-}
-printf("\n");
-}
+low = mid + 1;
 }
 return (-1);
 }
